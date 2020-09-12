cbccc56c074aac4ea12b934131faeec9667b88d2
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-12 22:38:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,9 +13,10 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language)
-  puts "Hi,"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def 
 
 
 
